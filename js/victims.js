9dcd8082f5026ba8dde8147e432557a8475f34f4
Stephanie Swearngin <stephanie.swearngin@csgazette.gazette.com>
2019-02-01 13:46:46 -0700

    commit message

---
+++ js/victims.js
@@ -0,0 +1,33 @@
+function allHomicides() {
+	return "Benjamin Lee Sharkey", "Shawnee Antonio Tapia", "Jerwarren Donterious Jackson", "Micah Lee Flick", "Manuel Zetina", "Darryl Tyrone Clark", "Corky Lee Oliver",
+"Michael David Porter", "Earnest Gene Ramsey II", "Larry Nelson", "Pamela Nelson", "Marcus Howard Denton", "Serena Lee Garcia", "Brandie Preciado", "Jesse Paul Schlegel",
+"Robert Glenn Maples", "Dionte Amon Harris", "Richard Paul Martinez", "Marshall Aaron Mitschelen", "Derrick Lamont Lawrence Jr.", "Michelle Ann Peters", "Brandon Scott Watkins",
+"Gustavo Del Sol Sanchez", "William Alexander Bacorn", "Paul Gannarelli", "Sophia Eliana Fundora", "Michael Dewayne Booker", "Terrell Cortez Madrid Duran", "Jordan Anthony Coleman",
+"Randol F. Lille", "Daniel Chamberlain Lehman", "Sheldon DeEmery King", "Raymond Daniel McCall", "Houston Malachai Cookson", "Kenyatta Vashon Horne", "Courtney Christine Jackson",
+"Judy Smith", "Jaylon Morgan", "Luis Molina", "Luis Starkey Chavez", "Jacquiline Wilde", "Shakir Cook-Troynel", "Candace R. Jones", "Cesar Hernandez Diaz", "Patrick Joseph Bustamante",
+"Nicholas Gabriel Romero", "Raymond Frank Maez II", "Brandon Thomas Coffin", "Robbie London";
+}
+
+function domesticViolence() {
+	return "Jerwarren Donterious Jackson", "Corky Lee Oliver", "Dionte Amon Harris", "Richard Paul Martinez", "Michelle Ann Peters",
+"Brandon Scott Watkins", "Courtney Christine Jackson", "Shakir Cook-Troynel", "Candace R. Jones";
+}
+
+function fatallyShot() {
+	return "Benjamin Lee Sharkey", "Shawnee Antonio Tapia", "Micah Lee Flick", "Manuel Zetina", "Darryl Tyrone Clark", "Corky Lee Oliver",
+"Michael David Porter", "Earnest Gene Ramsey II", "Marcus Howard Denton", "Serena Lee Garcia", "Brandie Preciado", "Jese Paul Schlegel",
+"Robert Glenn Maples", "Dionte Amon Harris", "Richard Paul Martinez", "Marshall Aaron Mitschelen", "Derrick Lamont Lawrence Jr.",
+"Michelle Ann Peters", "Brandon Scott Watkins", "Gustavo Del Sol Sanchez"; "William Alexander Bacorn", "Michael Dewayne Booker", "Terrell Cortez Madrid Duran",
+"Jordan Anthony Coleman", "Daniel Chamberlain Lehman", "Sheldon DeEmery King", "Raymond Daniel McCall", "Houston Malachai Cookson", "Kenyatta Vashon Horne",
+"Courtney Christine Jackson", "Jaylon Morgan", "Luis Starkey Chavez", "Candace R. Jones", "Cesar Hernandez Diaz", "Patrick Joseph Bustamante", "Nicholas Gabriel Romero",
+"Raymond Frank Maez II", "Brandon Thomas Coffin", "Robbie London";
+}
+
+function otherCauses() {
+	return "Jerwarren Donterious Jackson", "Larry Nelson", "Pamela Nelson", "Brandon Scott Watkins", "Paul Gannarelli", "Sophia Eliana Fundora",
+"Randol F. Lille", "Judy Smith", "Luis Molina", "Jacquiline Wilde";
+}
+
+function unsolvedHomicides() {
+	return "Darryl Clark", "Larry Nelson", "Pamela Nelson", "Richard Martinez", "Randol Lille", "Luis Molina", "Raymond Maez II", "Robbie London";
+}
